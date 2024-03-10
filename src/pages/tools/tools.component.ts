26aa3ecd0425ee8aee4bee599b8b9fcdc349757d
Martin René Sørensen <git@sicet7.com>
2024-03-10 12:43:44 +0100

    WIP.

--- src/pages/tools/tools.component.ts
+++ src/pages/tools/tools.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-tools',
@@ -6,5 +7,6 @@ import {Component} from '@angular/core';
     styleUrl: './tools.component.less'
 })
 export class ToolsComponent {
-
+    constructor(protected router: Router) {
+    }
 }
